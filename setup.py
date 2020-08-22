ab2abb193558dae3ded1f8276a9b83d4c3d6f743
Ivan Kondov <ivan.kondov@kit.edu>
2020-08-22 16:12:58 +0200

    reset the version using semantic versioning

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='fireworks_schema',
-    version='2020.2',
+    version='1.0.0',
     description='JSON Schema for FireWorks',
     long_description=long_description,
     long_description_content_type='text/markdown',
