0dd999f385ef8234f13a63d6c37faffa594a41cd
Ivan Kondov <ivan.kondov@kit.edu>
2025-06-22 11:20:29 +0200

    refactor code in json_schema.py

--- fireworks_schema/json_schema.py
+++ fireworks_schema/json_schema.py
@@ -15,35 +15,36 @@ import semantic_version
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 SCHEMA_DIR = os.path.join(ROOT_DIR, 'schema')
 
+
+def get_schema(schema_name):
+    """get schema dictionary and absolute path"""
+    schema_file = schema_name.lower() + '.json'
+    schema_path = os.path.join(SCHEMA_DIR, schema_file)
+    with open(schema_path, 'r', encoding='utf-8') as fileh:
+        schema_dict = json.load(fileh)
+    return schema_dict, os.path.abspath(schema_path)
+
+
 jsonschema_version = semantic_version.Version(version('jsonschema'))
 if jsonschema_version >= semantic_version.Version('4.18.0'):
     from referencing import Registry, Resource
 
     def validate_with_referencing(instance, schema_name):
-        """JSON schema validator working with relative paths: use referencing"""
-        schema_file = schema_name.lower() + '.json'
-        schema_path = os.path.join(SCHEMA_DIR, schema_file)
-        with open(schema_path, 'r', encoding='utf-8') as fileh:
-            schema_dict = json.load(fileh)
-        files = os.listdir(SCHEMA_DIR)
+        """JSON schema validator using the referencing package"""
+        schema_dict, _ = get_schema(schema_name)
         resources = []
-        for file_ in files:
+        for file_ in os.listdir(SCHEMA_DIR):
             with open(os.path.join(SCHEMA_DIR, file_), 'r', encoding='utf-8') as fp:
-                res = (file_, Resource.from_contents(json.load(fp)))
-            resources.append(res)
+                resources.append((file_, Resource.from_contents(json.load(fp))))
         registry = Registry().with_resources(resources)
         jsonschema.validate(instance, schema_dict, registry=registry,
                             format_checker=jsonschema.FormatChecker())
     validate = validate_with_referencing
 else:
     def validate_with_refresolver(instance, schema_name):
-        """JSON schema validator working with relative paths: use RefResolver"""
-        schema_file = schema_name.lower() + '.json'
-        schema_path = os.path.join(SCHEMA_DIR, schema_file)
-        with open(schema_path, 'r', encoding='utf-8') as fileh:
-            schema_dict = json.load(fileh)
-        base_uri = Path(os.path.abspath(schema_path)).as_uri()
-        custom_res = jsonschema.RefResolver(base_uri, schema_dict)
+        """JSON schema validator using RefResolver"""
+        schema_dict, schema_path = get_schema(schema_name)
+        custom_res = jsonschema.RefResolver(Path(schema_path).as_uri(), schema_dict)
         jsonschema.validate(instance, schema_dict, resolver=custom_res,
                             format_checker=jsonschema.FormatChecker())
     validate = validate_with_refresolver
